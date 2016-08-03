f8cdffb9014b2507d0b17fe98286fea10ddecc9f
aswartz04 <agsw@outlook.com>
2016-08-02 17:03:30 -0700

    Outline for astar procedure added

---
+++ astar.R
@@ -0,0 +1,32 @@
+a.star <- function(start,end,area){
+  closedS <- c();openS <- start
+  cameFrom <- c()
+  g.score <- NA
+  g.score[start] <- 0
+  f.score <- NA
+  f.score[start] <- 40
+  #while
+  while(length(openS) > 0){
+    current <- min(openS)
+    if(current==end){
+      reconstruct_path(cameFrom,current)
+    }
+    #remove current from openset
+    #add current to closed set
+    #loop for each neighboring node for current
+      #ignore neighbor that is already evaulated in closed set
+      #otherwise make a tentative gscore for the neighbors
+      #if the neighbor is not in the openset then add it
+      #otherwise if the tentative score is greater than the gscore, skip it
+      #now camefrom is set to current, gscore is the tentative score, fscore is this distance plus the heuristic
+  }
+  #visited
+  reconstruct_path <- function(cameFrom,current){
+    total.path <- current
+    while(current in cameFrom){
+      current <- cameFrom[cameFrom==current]
+      total.path <- c(total.path,current)
+    }
+    total.path
+  }
+}
